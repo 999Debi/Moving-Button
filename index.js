b444f2e2ce71fd635108057aa10493a10d1f4486
Debi <jenadebiprasad6@gmail.com>
2024-01-28 17:00:22 +0530

    cleared comments

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const btn=document.querySelector('.no');
 const img=document.querySelector('img');
 const yesbtn = document.querySelector(".yes");
 const text = document.querySelector("h1");
-// console.log(btn)
+
 
 btn.addEventListener("mouseover",()=>{
-    // console.log(7);
+   
     changePosition(btn,img);
 
 })
 yesbtn.addEventListener("click",()=>{
-    // console.log(7)
+   
     img.src = "https://i.postimg.cc/wTDG30TB/yes.gif";
     btn.style.display="none";
     yesbtn.style.display="none";
@@ -22,7 +22,7 @@ function changePosition(x) {
   x.style.position="absolute";
     let top= Math.floor(Math.random()*80);
     let left= Math.floor(Math.random()*80);
-    // transition: width 0.9s
+  
   x.style.top = `${top}%`;
   x.style.left = `${left}%`;
   
